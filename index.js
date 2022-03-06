b522ca6b22edeb15895827d60df6c4c9c172ec67
Vladyslav Smolianets <74509296+Just-Web-Developer@users.noreply.github.com>
2022-03-06 21:16:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 app.get('/targets', function(req, res) {
     res.send(JSON.stringify(
         {
-            targets:['http://fsb.ru', 'https://ria.ru/']
+            targets:['http://fsb.ru', 'http://ria.ru/']
         }
     ))
 })
